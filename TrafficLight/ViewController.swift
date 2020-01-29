b9ca7e0ddd71aa71aa945e9c38f2bf41d50b537c
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2020-01-29 16:53:11 +0100

    Created method for switch traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var firstLightView: UIView!
+  @IBOutlet var secondLightView: UIView!
+  @IBOutlet var thirdLightView: UIView!
+  @IBOutlet var switchButton: UIButton!
+  
+  let lightOpacity: CGFloat = 0.3
+  let fullLightOpacity: CGFloat = 1
+  let radiusOfLignt: CGFloat = 50
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    firstLightView.backgroundColor = UIColor.red
+    firstLightView.alpha = lightOpacity
+    firstLightView.layer.cornerRadius = radiusOfLignt
+    secondLightView.backgroundColor = UIColor.yellow
+    secondLightView.alpha = lightOpacity
+    secondLightView.layer.cornerRadius = radiusOfLignt
+    thirdLightView.backgroundColor = UIColor.green
+    thirdLightView.alpha = lightOpacity
+    thirdLightView.layer.cornerRadius = radiusOfLignt
   }
 
-
+  @IBAction func changeColorButton() {
+    switchButton.setTitle("Next", for: .normal)
+    if (firstLightView.alpha != fullLightOpacity && secondLightView.alpha != fullLightOpacity || thirdLightView.alpha == fullLightOpacity) {
+      firstLightView.alpha = fullLightOpacity
+      thirdLightView.alpha = lightOpacity
+    } else if (firstLightView.alpha == fullLightOpacity) {
+      secondLightView.alpha = fullLightOpacity
+      firstLightView.alpha = lightOpacity
+    } else if (secondLightView.alpha == fullLightOpacity){
+      secondLightView.alpha = lightOpacity
+      thirdLightView.alpha = fullLightOpacity
+    }
+  }
+  
+  
 }
 
