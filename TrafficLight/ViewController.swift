30b26bebdbb4f84fbfd85e09feaf8e00f22cb749
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2020-01-30 22:48:46 +0100

    Fixes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,44 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  @IBOutlet var firstLightView: UIView!
-  @IBOutlet var secondLightView: UIView!
-  @IBOutlet var thirdLightView: UIView!
-  @IBOutlet var switchButton: UIButton!
-  
-  let lightOpacity: CGFloat = 0.3
-  let fullLightOpacity: CGFloat = 1
-  let radiusOfLignt: CGFloat = 50
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    firstLightView.backgroundColor = UIColor.red
-    firstLightView.alpha = lightOpacity
-    firstLightView.layer.cornerRadius = radiusOfLignt
-    secondLightView.backgroundColor = UIColor.yellow
-    secondLightView.alpha = lightOpacity
-    secondLightView.layer.cornerRadius = radiusOfLignt
-    thirdLightView.backgroundColor = UIColor.green
-    thirdLightView.alpha = lightOpacity
-    thirdLightView.layer.cornerRadius = radiusOfLignt
-  }
-
-  @IBAction func changeColorButton() {
-    switchButton.setTitle("Next", for: .normal)
-    if (firstLightView.alpha != fullLightOpacity && secondLightView.alpha != fullLightOpacity || thirdLightView.alpha == fullLightOpacity) {
-      firstLightView.alpha = fullLightOpacity
-      thirdLightView.alpha = lightOpacity
-    } else if (firstLightView.alpha == fullLightOpacity) {
-      secondLightView.alpha = fullLightOpacity
-      firstLightView.alpha = lightOpacity
-    } else if (secondLightView.alpha == fullLightOpacity){
-      secondLightView.alpha = lightOpacity
-      thirdLightView.alpha = fullLightOpacity
-    }
-  }
-  
-  
+enum CurrentLight {
+	case red
+	case yellow
+	case green
 }
 
+class ViewController: UIViewController {
+	
+	@IBOutlet var firstLightView: UIView!
+	@IBOutlet var secondLightView: UIView!
+	@IBOutlet var thirdLightView: UIView!
+	@IBOutlet var switchButton: UIButton!
+	
+	private var currentLight = CurrentLight.red
+	
+	private let lightOpacity: CGFloat = 0.3
+	private let fullLightOpacity: CGFloat = 1
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		firstLightView.backgroundColor = UIColor.red
+		firstLightView.alpha = lightOpacity
+		secondLightView.backgroundColor = UIColor.yellow
+		secondLightView.alpha = lightOpacity
+		thirdLightView.backgroundColor = UIColor.green
+		thirdLightView.alpha = lightOpacity
+	}
+	
+	// If demensions Lights (UIView) depends of screen-size, we need to use method viewDidLayoutSubviews(), because method viewDidLoad() dont know about screen-size, this method tooks size that i use in main.storyboards
+	override func viewDidLayoutSubviews() {
+		firstLightView.layer.cornerRadius = firstLightView.frame.width / 2
+		secondLightView.layer.cornerRadius = secondLightView.frame.width / 2
+		thirdLightView.layer.cornerRadius = thirdLightView.frame.width / 2
+	}
+	
+	@IBAction func changeColorButton() {
+		switchButton.setTitle("Next", for: .normal)
+		
+		switch currentLight {
+		case .red:
+			firstLightView.alpha = fullLightOpacity
+			thirdLightView.alpha = lightOpacity
+			currentLight = .yellow
+		case .yellow:
+			firstLightView.alpha = lightOpacity
+			secondLightView.alpha = fullLightOpacity
+			currentLight = .green
+		case .green:
+			secondLightView.alpha = lightOpacity
+			thirdLightView.alpha = fullLightOpacity
+			currentLight = .red
+		}
+		
+//		if (firstLightView.alpha != fullLightOpacity && secondLightView.alpha != fullLightOpacity || thirdLightView.alpha == fullLightOpacity) {
+//			firstLightView.alpha = fullLightOpacity
+//			thirdLightView.alpha = lightOpacity
+//		} else if (firstLightView.alpha == fullLightOpacity) {
+//			secondLightView.alpha = fullLightOpacity
+//			firstLightView.alpha = lightOpacity
+//		} else if (secondLightView.alpha == fullLightOpacity){
+//			secondLightView.alpha = lightOpacity
+//			thirdLightView.alpha = fullLightOpacity
+//		}
+	}
+	
+	
+}
